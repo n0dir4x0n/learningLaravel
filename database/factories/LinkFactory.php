0e50a2c4e1735540648a46402a621910dbf62ed3
Javokhir Achilov <javoxiraka@gmail.com>
2018-06-12 18:39:23 +0500

    feat(auth): created

--- database/factories/LinkFactory.php
+++ database/factories/LinkFactory.php
@@ -4,6 +4,8 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Link::class, function (Faker $faker) {
     return [
-        //
+        'title' => substr($faker->sentence(2), 0, -1),
+        'url' => $faker->url,
+        'description' => $faker->paragraph,
     ];
 });
--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,30 @@
 |
 */
 
+use Illuminate\Http\Request;
+
 Route::get('/', function () {
-    return view('welcome');
+    $links = \App\Link::all();
+
+    return view('welcome', ['links' => $links]);
 });
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::post('/submit', function (Request $request) {
+    $data = $request->validate([
+        'title' => 'required|max:255',
+        'url' => 'required|url|max:255',
+        'description' => 'required|max:255',
+    ]);
+    
+    $link = new \App\Link;
+    $link->title = $data['title'];
+    $link->url = $data['url'];
+    $link->description = $data['description'];
+    
+    // Save the model
+    $link->save();
+});
